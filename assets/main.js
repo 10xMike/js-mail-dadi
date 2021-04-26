ccb607d443f140285374bd2ea5675d8c502a09b0
10xMike <michelegec@gmail.com>
2021-04-26 17:17:09 +0200

    Push finale corretto

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,30 @@
 // Chiedi all’utente la sua email
-    var mail = prompt("Qual è la tua mail?");
+var mail = prompt("Qual è la tua mail?"); 
+var access = false;
 
-// controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo
+// Controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo
+var mailArray = ["ottavio@gmail.com", "giuliano@gmail.com", "franchino@gmail.com", "michele@gmail.com", "manfredi@gmail.com"];
 
-if (mail == "michele@gmail.com"){
-    document.getElementById("email").innerHTML = "Access Granted";
+for (var index = 0; index < mailArray.length; index++) {
+    if (mail == mailArray[index]) {
+        access = true;
+    }
 }
 
+if (access == true) {
+    document.getElementById('email').innerHTML = "La tua mail corrisponde"
+} else {
+    document.getElementById('email').innerHTML = "La tua mail non corrisponde"
+
+}
+
+
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
-    var playerdicenumber = Math.floor( Math.random() * 6 ) +1;
+    var playerdicenumber = Math.floor( Math.random() * 6 ) + 1;
     console.log(playerdicenumber);
     document.getElementById("playerdicenumber").innerHTML = "Il risultato del tuo dado è " + playerdicenumber;
 
-    var computerdicenumber = Math.floor( Math.random() * 6) +1;
+    var computerdicenumber = Math.floor( Math.random() * 6) + 1;
     console.log(computerdicenumber);
     document.getElementById("computerdicenumber").innerHTML = "Il risultato del dado del PC è " + computerdicenumber;
 
@@ -28,3 +40,6 @@ if (playerdicenumber > computerdicenumber) {
     console.log("you lose")
     document.getElementById("game").innerHTML = "You Lose"
   }
+
+  // aggiungi array mail, esegui ciclo for sull'array mail, + if (mail = elemento nell'array)
+  // variabile sentinella (trovato; false; true)
